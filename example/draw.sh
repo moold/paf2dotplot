e3a5764e1c06790ecdb06353ec19424e69569b34
Huj <mooldhu@gmail.com>
2024-12-20 15:02:08 +0800

    update breakpoints

--- example/draw.sh
+++ example/draw.sh
@@ -1,2 +1,4 @@
 ../paf2dotplot.r -f -b --plot-size 10 test1.paf
 ../paf2dotplot.r -f -s --plot-size 10 test2.paf
+# convert -density 100 test1.paf.pdf test1.paf.png
+# convert -density 100 test2.paf.pdf test2.paf.png
